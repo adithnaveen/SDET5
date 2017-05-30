ee2ed70cb532b2ce5e6b13a04c78ef618399d38b
naveen <adith.naveen@gmail.com>
2017-05-30 13:30:40 -0700

    added new statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,7 @@ package com.naveen;
 public class HelloWorld{
 	public static void main(String [] args){
 		System.out.println("Hello World from Java to git throught Jenkins");
+		
+		System.out.println("This is additional line");
 	}
 }
