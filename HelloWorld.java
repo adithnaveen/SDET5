e10aafe13f5520dc982ae9657bb87695550922b5
naveen <adith.naveen@gmail.com>
2017-06-09 13:03:21 -0700

    Create a emp class in hello world for testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,13 +7,30 @@ package com.fannie;
 // to Execute 
 // java com.fannie.HelloWorld
 
+
+class Emp {
+	 int empId;
+	String empName;
+	
+}
 public class HelloWorld{
 
 	public static void main(String args[]){
 		System.out.println("This is from Notepad ");
 
+		Emp emp = new Emp();
+		emp.empId = 22; 
+		emp.empName = "harry";
+		
+		
 		for(int i=0; i<10; i+=2){
 			System.out.println(i);
 		}
+		
+		System.out.prinltn("----------------------------");
+		System.out.prinltn("Emp Id " + emp.empId);
+		System.out.println("Emp Name " + emp.empName);
+		
+		
 	}
 }
