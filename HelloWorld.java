8b7354c3bba1ca1a018a2817e02eb4054df79abd
naveen <adith.naveen@gmail.com>
2017-06-09 12:57:15 -0700

    Added HelloWorld in class

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+package com.fannie;
+
+
+// to compile 
+// javac -d . HelloWorld.java
+
+// to Execute 
+// java com.fannie.HelloWorld
+
+public class HelloWorld{
+
+	public static void main(String args[]){
+		System.out.println("This is from Notepad ");
+
+		for(int i=0; i<10; i+=2){
+			System.out.println(i);
+		}
+	}
+}
